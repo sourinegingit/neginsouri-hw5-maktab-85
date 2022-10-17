b84cff56708784986894a7fd1cd624ea08871ed1
negin souri <nsouri7979@gmail.com>
2022-10-17 08:49:45 +0430

    Solve the first question

---
+++ question1/script.js
@@ -0,0 +1,25 @@
+// question 1
+// first question give method in object
+// let user={
+//         name:"jhon",
+//         age:30,
+//         sayHello:function(){
+//             return  `hello ${this.name}`
+//         },
+//     };
+// console.log(user.sayHello());
+
+
+// second way
+// let user={
+//     name:"jhon",
+//     age:30,
+//     sayHello:"",
+// }
+// let call=user.sayHello.concat("hello jhon");
+// // document.write(call);
+// console.log(call)
+
+
+// --------------------
+// question 2
